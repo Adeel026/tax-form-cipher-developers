a8fadc99eef2c48b5214d1d3068d2465732c6685
Adeel026 <madeel5309@gmail.com>
2024-07-21 18:39:26 +0500

    initial push

---
+++ tax-form/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Taxform from './components/js file/taxform';
+function App() {
+  return (
+    <div className="App">
+   <Taxform/>
+    </div>
+  );
+}
+
+export default App;
